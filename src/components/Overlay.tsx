5373de4e29e6f30cb28f5ab35a266e0ab0c13eb4
Johan Lundgren <johan@banduri.se>
2024-05-25 17:29:46 +0200

    added missed styling

--- src/components/Overlay.tsx
+++ src/components/Overlay.tsx
@@ -13,7 +13,7 @@ export default function Overlay() {
         <div>
           <h1 className="text-white font-bold text-5xl leading-[48px] -tracking-[1px]">Treat</h1>
           <h1 className="text-white font-bold text-5xl leading-[48px] -tracking-[1px]">yourself.</h1>
-          <p className="text-white text-[14px] leading-[21px] -tracking-[0.5px]">Find the best restaurants in your city </p>
+          <p className="text-white text-[14px] leading-[21px] -tracking-[0.5px] mt-4">Find the best restaurants in your city </p>
           <p className="text-white text-[14px] leading-[21px] -tracking-[0.5px]">and get it delivered to your place!</p>
         </div>
         <button onClick={() => setShow(false)} className="rounded-lg w-[327px] border-[1px] py-5 mx-auto mb-10 font-bold text-[16px] leading-[16px] -tracking-[0.5px] text-white">
