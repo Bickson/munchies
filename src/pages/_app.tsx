5f0cacce06d3ebeedfda1eda5636388df8681ffa
Johan Lundgren <johan@banduri.se>
2024-05-25 17:18:12 +0200

    fixed font for safari

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import localFont from 'next/font/local'
-const myFont = localFont({ src: '../../public/fonts/SF-Pro.ttf' })
+const myFont = localFont({ src: '../../public/fonts/SF-Pro.woff' })
 
 export default function App({ Component, pageProps }: AppProps) {
   return <main className={myFont.className}>
